0977e855ed4d3bcb669039a07aa6721c35b92a08
tanyang1767774755 <1767774755@qq.com>
2021-01-15 20:44:31 +0800

    1-4

---
+++ 1_4/1_4/test.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int x = 10, y = 5;
+//	switch (x)
+//	{
+//	case 1:x++;
+//	default:x+= y;
+//	case 2:y--;
+//	case 3:x--;
+//	}
+//	printf("x = %d, y = %d", x, y);
+//	return 0;
+//}
+int main()
+{
+	int x=023;
+	printf("%o",x);
+	//scanf("%d",&x);
+	//switch (x)
+	//{
+	//case 1:printf("1");
+	//default:printf("4");
+	//case 2:printf("2");
+	//
+	//}
+}
